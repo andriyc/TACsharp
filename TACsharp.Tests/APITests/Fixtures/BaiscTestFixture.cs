6924738047c23c745715d84bcba3d494dcdf4f62
andriy_chornyi <achor2@softserveinc.com>
2022-01-20 14:39:42 +0200

    Initial Solution Creation

---
+++ TACsharp.Tests/APITests/Fixtures/BaiscTestFixture.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TACsharp.Tests.APITests.Fixtures
+{
+    public class BaiscTestFixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
