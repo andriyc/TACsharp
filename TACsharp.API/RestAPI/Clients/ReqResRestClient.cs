884642000f32fef4b9dce85b99571e9ea5878fdb
andriy_chornyi <achor2@softserveinc.com>
2022-01-21 14:31:46 +0200

    started ReqResRestClient class implementation - API level

---
+++ TACsharp.API/RestAPI/Clients/ReqResRestClient.cs
@@ -0,0 +1,47 @@
+﻿using TACsharp.Framework.Core.REST;
+
+namespace TACsharp.API.RestAPI.Clients
+{
+    /// <summary>
+    /// https://reqres.in/ REST client class
+    /// </summary>
+    public class ReqResRestClient
+    {
+        private const string BaseUrl = "https://reqres.in";
+        private const string UserListSource = "/api/users";
+
+        private readonly RESTClient _client;
+
+        private ReqResRestClient()
+        {
+            _client = RESTClient.NewClient(BaseUrl);
+        }
+
+        /// <summary>
+        /// Creates and returns new ReqResRestClint object
+        /// </summary>
+        /// <returns>new ReqResRestClient object</returns>
+        public static ReqResRestClient New()
+        {
+            return new ReqResRestClient();
+        }
+
+        /// <summary>
+        /// Gets User list via API
+        /// </summary>
+        public RESTResponse GetUserList()
+        {
+            var request = RESTRequest.GET(UserListSource);
+            return _client.ExecuteAsync(request).Result;
+        }
+
+        /// <summary>
+        /// Gets User by ID via API
+        /// </summary>
+        public RESTResponse GetUserByID(int userID)
+        {
+            var request = RESTRequest.GET(UserListSource + $"/{userID}");
+            return _client.ExecuteAsync(request).Result;
+        }
+    }
+}
