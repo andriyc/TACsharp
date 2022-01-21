b3f68225f99ce80663adbbfe3965eb4dd64630af
andriy_chornyi <achor2@softserveinc.com>
2022-01-21 14:36:01 +0200

    Temporary implementation of Test Fixture for Solution testing

--- TACsharp.Tests/APITests/Fixtures/BaiscTestFixture.cs
+++ TACsharp.Tests/APITests/Fixtures/BaiscTestFixture.cs
@@ -1,9 +1,19 @@
 using NUnit.Framework;
+using System.Threading.Tasks;
+using TACsharp.API.RestAPI.Clients;
 
 namespace TACsharp.Tests.APITests.Fixtures
 {
     public class BaiscTestFixture
     {
+        private ReqResRestClient ReqResClient;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        { 
+            ReqResClient = ReqResRestClient.New();
+        }
+
         [SetUp]
         public void Setup()
         {
@@ -12,6 +22,9 @@ namespace TACsharp.Tests.APITests.Fixtures
         [Test]
         public void Test1()
         {
+            var response1 = ReqResClient.GetUserList();
+            var response3 = ReqResClient.GetUserByID(2);
+
             Assert.Pass();
         }
     }
