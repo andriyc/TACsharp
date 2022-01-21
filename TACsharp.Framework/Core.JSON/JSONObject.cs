4b225618e63532dcef95129446987b1dbc625873
andriy_chornyi <achor2@softserveinc.com>
2022-01-21 23:27:13 +0200

    implemnted JSONObject wrapper class

---
+++ TACsharp.Framework/Core.JSON/JSONObject.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TACsharp.Framework.Core.JSON
+{
+    public class JSONObject
+    {
+        private JObject _json;
+
+        private JSONObject(JObject json)
+        {
+            _json = json;
+        }
+
+        public static JSONObject Parse(string json)
+        {
+            return new JSONObject(JObject.Parse(json));
+        }
+
+        public T ToObject<T>()
+        {
+            return _json.ToObject<T>();
+        }
+
+    }
+}
