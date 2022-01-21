2f2a83b0d5efd642655de4d9a61e5009c6efd12e
andriy_chornyi <achor2@softserveinc.com>
2022-01-21 14:27:15 +0200

    Started REST response wrapper implementation - Framework level

---
+++ TACsharp.Framework/Core.REST/RESTResponse.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace TACsharp.Framework.Core.REST
+{
+    /// <summary>
+    /// REST response wrapper class
+    /// </summary>
+    public sealed class RESTResponse
+    {
+        private RestResponse _response;
+
+        public string Content => _response.Content;
+        public HttpStatusCode StatusCode => _response.StatusCode;
+
+        public RESTResponse(RestResponse response)
+        {
+            _response = response;
+        }
+    }
+}
