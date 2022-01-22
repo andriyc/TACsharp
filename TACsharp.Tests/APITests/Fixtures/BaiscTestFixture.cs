7a755cca8d85815f2f7176281bd388970e0cb798
andriy_chornyi <achor2@softserveinc.com>
2022-01-22 20:25:48 +0200

    implemnted DataList parsing testing

--- TACsharp.Tests/APITests/Fixtures/BaiscTestFixture.cs
+++ TACsharp.Tests/APITests/Fixtures/BaiscTestFixture.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System.Threading.Tasks;
 using TACsharp.API.RestAPI.Clients;
 using TACsharp.API.RestAPI.Models;
 using TACsharp.Framework.Core.JSON;
@@ -24,26 +23,34 @@ namespace TACsharp.Tests.APITests.Fixtures
         [Test]
         public void Test1()
         {
-            var userList = ReqResClient.GetUserList();
+            var userListResponse = ReqResClient.GetUserList();
+            var userList = JSONObject
+                                .Parse(userListResponse.Content)
+                                .ToObject<DataList<ReqResUser>>()
+                                .Data;
 
             var userID2 = 2;
             var userWithUserID2 = ReqResClient.GetUserByID(userID2);
             var user = JSONObject
-                        .Parse(userWithUserID2.Content)
-                        .ToObject<ReqResResponse<ReqResUser>>()
-                        .Data;
+                                .Parse(userWithUserID2.Content)
+                                .ToObject<ReqResResponse<ReqResUser>>()
+                                .Data;
 
             var userIDNA = 23;
             var userNA = ReqResClient.GetUserByID(userIDNA);
 
-            var resourceList = ReqResClient.GetResourceList();
+            var resourceListResponse = ReqResClient.GetResourceList();
+            var resourseList = JSONObject
+                                .Parse(resourceListResponse.Content)
+                                .ToObject<DataList<ReqResResource>>()
+                                .Data;
 
             var resourceID2 = 2;
             var singleResourceWithID2 = ReqResClient.GetResourceByID(resourceID2);
             var resource = JSONObject
-                            .Parse(singleResourceWithID2.Content)
-                            .ToObject<ReqResResponse<ReqResResource>>()
-                            .Data;
+                                .Parse(singleResourceWithID2.Content)
+                                .ToObject<ReqResResponse<ReqResResource>>()
+                                .Data;
 
             var resourceIDNA = 23;
             var resourceNA = ReqResClient.GetResourceByID(resourceIDNA);
