faf12e07d0252299b02a77c306aa068aa2c26cdd
andriy_chornyi <achor2@softserveinc.com>
2022-01-25 21:49:49 +0200

    added Response model

---
+++ TACsharp.API/RestAPI/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace TACsharp.API.RestAPI.Models
+{
+    /// <summary>
+    /// ReqRes response model
+    /// </summary>
+    /// <typeparam name="T">The type of data container in Response</typeparam>
+    public class Response<T>
+    {
+        [JsonProperty("data")]
+        public T Data { get; set; }
+        
+        //Support - for further implementation if needed
+    }
+}
