bd59cd76ec22c77ff3a4a834c2b7f49ac9e93cb5
andriy_chornyi <achor2@softserveinc.com>
2022-01-26 12:55:06 +0200

    updated API data models

---
+++ TACsharp.API/RestAPI/Models/ReqRes/NewUserResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TACsharp.API.RestAPI.Models.ReqRes
+{
+    public class NewUserResponse : NewUserRequest
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("createdAt")]
+        public string CreatedAt { get; set; }
+    }
+}
