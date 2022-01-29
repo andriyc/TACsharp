fb5e62051b19cbd03ef046f65972a3d3c18e7342
andriy_chornyi <achor2@softserveinc.com>
2022-01-29 19:44:05 +0200

    implemented RegisterUser data models

---
+++ TACsharp.API/RestAPI/Models/ReqRes/RegisterUserRequest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TACsharp.API.RestAPI.Models.ReqRes
+{
+    public class RegisterUserRequest
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        public static RegisterUserRequest Create(string email, string password)
+        {
+            return new RegisterUserRequest()
+            {
+                Email = email,
+                Password = password
+            };
+        }
+    }
+}
---
+++ TACsharp.API/RestAPI/Models/ReqRes/RegisteredUserResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TACsharp.API.RestAPI.Models.ReqRes
+{
+    public class RegisteredUserResponse
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
