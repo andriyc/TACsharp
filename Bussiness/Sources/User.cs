0b7c58ca8e4c7b3c2b893c00fcfe080a7bcf261e
andriy_chornyi <achor2@softserveinc.com>
2022-02-09 17:24:05 +0200

    started User clas implementation on Bussiness level

---
+++ Bussiness/Sources/User.cs
@@ -0,0 +1,25 @@
+﻿
+using TACsharp.API.RestAPI.Interfaces;
+
+namespace Bussiness.Sources
+{
+    public class User
+    {
+        private IRestClient _client;
+
+        private User(IRestClient client)
+        {
+            _client = client;
+        }
+
+        public static User New(IRestClient client)
+        {
+            return new User(client);
+        }
+
+        public object Create(string user, string job)
+        {
+            return _client.CreateUser(user, job);
+        }
+    }
+}
